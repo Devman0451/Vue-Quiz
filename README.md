e14f06d50e2ceab3c1cc256183e3563023bbbf13
Justin Myers <42520471+Devman0451@users.noreply.github.com>
2019-05-23 19:25:57 -0400

    commit

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-# quiz
+# Vue Quiz
+
+This was built as a project for [FreeCodeCamp's](https://www.https://www.youtube.com/watch?v=4deVCNJq3qc.org/) Learn Vue Course.  I also fixed some minor bugs with the finished project.
+
+## Bugs Fixed
+- The app would index past the number of questions fetched from the API.
+- HTML special entities weren't being parsed from the fetched questions.
+- No way to start the quiz over once all 10 questions were finished.
+- Next button was still active after all 10 questions were finished
 
 ## Project setup
 ```
@@ -25,5 +33,11 @@ npm run test
 npm run lint
 ```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+## Built With
+
+* Vue
+* Redux
+
+## Contributors
+
+* **Justin Myers** - [My Portfolio](https://tender-varahamihira-ff1ef7.netlify.com/)
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,84 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header 
+      :numCorrect="numCorrect"
+      :numTotal="numTotal"
+    />
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="6" offset="3">
+          <QuestionBox
+            v-if="questions.length"
+            :currentQuestion="questions[index]"
+            :next="next"
+            :increment="increment"
+            :done="done"
+           />
+          <b-button 
+            @click="restartQuiz" 
+            variant="danger"
+            :disabled="!done"
+          >
+          Start A New Quiz
+        </b-button>
+        </b-col>
+      </b-row>
+</b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import QuestionBox from './components/QuestionBox.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header,
+    QuestionBox
+  },
+  data() {
+    return {
+      questions: [],
+      index: 0,
+      numCorrect: 0,
+      numTotal: 0,
+      done: false
+    }
+  },
+  methods: {
+    next() {
+      if(this.index >= this.questions.length - 1) {
+        this.done = true;
+      } else {
+        this.index++
+      }
+    },
+    increment(isCorrect) {
+      if (isCorrect) {
+        this.numCorrect++
+      } 
+      this.numTotal++
+    },
+    fetchData() {
+      fetch('https://opentdb.com/api.php?amount=10&type=multiple', {
+        method: 'get'
+      })
+      .then(res => res.json())
+      .then(data => {
+        this.questions = data.results
+      })
+    },
+    restartQuiz() {
+      this.index = 0;
+      this.numCorrect = 0
+      this.numTotal = 0
+      this.done = false
+      this.fetchData()
+    }
+  },
+  mounted: function() {
+    this.fetchData();
   }
 }
 </script>
